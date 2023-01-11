f51c7a8e58c7f59db3a6ee22daa0ef6c503878cf
trab <77131432+trab-ml@users.noreply.github.com>
2023-01-11 16:48:14 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -29,8 +29,31 @@ $(document).ready(function(){
       'linear'
     );
   });
+
   $('a[id="download-link"]').click(function(e) {
     e.preventDefault(); 
     window.location.href = 'moncv.pdf';
   }); 
+
+  let popup_div = [ 
+    {name: "#popup1", trigger: "#open-popup1", closer: "#close-popup1"},
+    {name: "#popup2", trigger: "#open-popup2", closer: "#close-popup2"},
+    {name: "#popup3", trigger: "#open-popup3", closer: "#close-popup3"},
+    {name: "#popup4", trigger: "#open-popup4", closer: "#close-popup4"},
+    {name: "#popup5", trigger: "#open-popup5", closer: "#close-popup5"},
+    {name: "#popup6", trigger: "#open-popup6", closer: "#close-popup6"}
+  ];
+  var popup_name = "", popup_trigger = "", popup_closer = "";
+  
+  //not finished
+  popup_div.forEach(popup => {
+    popup_name = popup.name;
+    popup_trigger = popup.trigger;
+    popup_closer = popup.closer;
+    // console.log(popup_name, popup_trigger, popup_closer);
+  
+    // Hide the pop-up on page load
+    $(popup_name).hide();
+  })
+
 });
