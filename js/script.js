84ac5dca84bc0a01d3267862784d8c9c088da023
trab-ml <btraore25@yahoo.com>
2023-07-18 01:01:43 +0200

    some cleaning

--- js/script.js
+++ js/script.js
@@ -1,53 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $("#menu").click(function () {
+        $(this).toggleClass("fa-times");
+        $("header").toggleClass("toggle");
+    });
 
+    $(window).on("scroll load", function () {
+        $("#menu").removeClass("fa-times");
+        $("header").removeClass("toggle");
 
-  $('#menu').click(function(){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');
-  });
+        if ($(window).scrollTop() > 0) {
+            $(".top").show();
+        } else {
+            $(".top").hide();
+        }
+    });
 
-  $(window).on('scroll load',function(){
+    // handle click
+    $(".nav-link").click(function (e) {
+        if ($(window).width() < 500) {
+            // $("#sidebar").removeClass("show");
+            $("#sidebar").css({'display': 'none'});
+        }
+        $(".nav-link").css({
+          'color': '#000',
+          'background': '#fff'
+        });
+        $(this).addClass("active");
+        $(this).css({
+          'color': 'beige',
+          'background': '#00bcd4'
+        });
+        console.log($(this));
+    });
 
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
+    // smooth scrolling
+    $('a[href*="#"]').on("click", function (e) {
+        e.preventDefault();
 
-    if($(window).scrollTop() > 0){
-      $('.top').show();
-    }else{
-      $('.top').hide();
-    }
+        $("html, body").animate(
+            {
+                scrollTop: $($(this).attr("href")).offset().top,
+            },
+            500,
+            "linear"
+        );
+    });
 
-  });
+    $('a[id="download-link"]').click(function (e) {
+        e.preventDefault();
+        window.location.href = "cv.pdf";
+    });
 
-  // smooth scrolling 
-  $('a[href*="#"]').on('click',function(e){
-    e.preventDefault();
+    // scroll animation
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
+    });
 
-    $('html, body').animate({
-        scrollTop : $($(this).attr('href')).offset().top,
-      },
-      500, 
-      'linear'
-    );
-  });
-
-  $('a[id="download-link"]').click(function(e) {
-    e.preventDefault(); 
-    window.location.href = 'cv.pdf';
-  }); 
-
-  // scroll animation
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if(entry.isIntersecting) {
-        entry.target.classList.add('show')
-      }
-      else {
-        entry.target.classList.remove('show')
-      }
-    })
-  })
-
-  const hiddenElements = document.querySelectorAll('.hidden')
-  hiddenElements.forEach((el) => observer.observe(el))
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
 });
